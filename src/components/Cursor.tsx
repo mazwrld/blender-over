97e29fcf18f3a2c95ec95b15424a807fc1436d2b
Maz <mazspm@icloud.com>
2024-03-19 17:39:30 -0500

    done

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,38 +1,40 @@
 import gsap from 'gsap'
 import { useEffect, useRef } from 'react'
+import { linearInterpolation } from '../../utils/linearInterpolation'
+
+const colors = ['#c32d27', '#f5c63f', '#457ec4', '#6638f0']
 
 export default function Cursor({ isHovered }: { isHovered: boolean }) {
   const size = isHovered ? 400 : 40
   const mouse = useRef({ x: 0, y: 0 })
   const delayedMouse = useRef({ x: 0, y: 0 })
-  const circle = useRef<HTMLDivElement | null>(null)
+  const circles = useRef<(HTMLDivElement | null)[]>([])
 
   function manageMouseMove(event: MouseEvent) {
     const { clientX, clientY } = event
     mouse.current = { x: clientX, y: clientY }
   }
 
-  const linearInterpolation = (
-    initialValue: number,
-    target: number,
-    amount: number
-  ) => initialValue * (1 - amount) + target * amount
-
-  function moveCircle(x: number, y: number) {
-    if (circle.current) {
-      gsap.set(circle.current, { x, y, xPercent: -50, yPercent: -50 })
-    }
+  function moveCircles(x: number, y: number) {
+    circles.current.forEach((circle) => {
+      if (circle) {
+        gsap.set(circle, {
+          x,
+          y,
+          xPercent: -50,
+          yPercent: -50,
+        })
+      }
+    })
   }
 
   function animate() {
     const { x, y } = delayedMouse.current
-
     delayedMouse.current = {
       x: linearInterpolation(x, mouse.current.x, 0.075),
       y: linearInterpolation(y, mouse.current.y, 0.075),
     }
-
-    moveCircle(delayedMouse.current.x, delayedMouse.current.y)
+    moveCircles(delayedMouse.current.x, delayedMouse.current.y)
     window.requestAnimationFrame(animate)
   }
 
@@ -43,16 +45,25 @@ export default function Cursor({ isHovered }: { isHovered: boolean }) {
   }, [])
 
   return (
-    <div
-      ref={circle}
-      className="fixed top-0 left-0 bg-[#BCE4F2] rounded-full mix-blend-difference pointer-events-none"
-      style={{
-        width: size,
-        height: size,
-        filter: isHovered ? 'blur(30px)' : 'none',
-        transition:
-          'height 0.3s ease-out, width 0.3s ease-out, filter 0.3s ease-out',
-      }}
-    />
+    <>
+      {colors.map((color, index, array) => {
+        return (
+          <div
+            key={index}
+            ref={(ref) => (circles.current[index] = ref)}
+            className="fixed top-0 left-0 rounded-full mix-blend-difference pointer-events-none"
+            style={{
+              backgroundColor: color,
+              width: size,
+              height: size,
+              filter: isHovered ? 'blur(30px)' : '2px',
+              transition: `height 0.3s ease-out, width 0.3s ease-out, filter 0.3s ease-out, transform ${
+                (array.length - index) * 0.095
+              }s ease-out`,
+            }}
+          />
+        )
+      })}
+    </>
   )
 }
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -12,7 +12,7 @@ export default function Scene() {
         onMouseLeave={() => {
           setIsHovered(false)
         }}
-        className="text-[4.5vw] max-w-[90vw] text-center text-white p-20"
+        className="text-[4.5vw] max-w-[90vw] text-center text-white p-20 z-20"
       >
         The quick brown fox jumps over the lazy dog
       </h1>
